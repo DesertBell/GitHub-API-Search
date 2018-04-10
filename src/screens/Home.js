3dc18bf988a6499ddb2d4c01e8d22758949f0057
Chele B <DesertBell@users.noreply.github.com>
2018-04-09 18:09:23 -0700

    Add files via upload

---
+++ src/screens/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../components/Header';
+
+//const Home = () => <Header title="Home" />
+
+export default React.createClass({
+    render() {
+        return <div> 
+                    <Header title="Home"/>
+                    Hello 
+                </div>
+    }
+})
+
+// export default Home;
---
+++ src/screens/Repos.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Header from '../components/Header';
+import {Provider} from 'react-redux';
+
+import configureStore from '../store/configureStore';
+
+import ItemList from '../components/ItemList';
+
+//const Repos = () => <Header title="Repos" />
+
+const store = configureStore();
+
+export default React.createClass({
+    render() {
+        return (
+            <div> 
+            <Header title="Repos"/>  
+        <Provider store={store}>
+           
+            <ItemList />
+        </Provider>
+        </div>        
+        )
+    }
+})
+
+
